89f9474c726a8229670d82947bd5817e9afb907c
chicainoportuna <paula.mara@gmail.com>
2019-01-13 21:32:04 +0100

    Add to docs

---
+++ docs/precache-manifest.372f5c893e91ba31a8847912bb170c92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "4b3c6affded90f137d4a",
+    "url": "./static/js/main.4b3c6aff.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "./static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "4b3c6affded90f137d4a",
+    "url": "./static/css/main.b1881a47.chunk.css"
+  },
+  {
+    "revision": "cbaaf5e2141d263e3775ffc7c00f7c6d",
+    "url": "./index.html"
+  }
+];
